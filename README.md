fefa19563c5cef3d3644ed9ba83ea4a34c19ce05
Kapareliotis Elias (Καπαρελιώτης Ηλίας) <where-at@helpis.dev>
2023-08-19 19:12:50 +0300

    feat: Move resizable text to seperate package.

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ For general information about developing packages, see the Dart guide for
 and the Flutter guide for
 [developing packages and plugins](https://flutter.dev/developing-packages).
 -->
+# resizable_text
 
 TODO: Put a short description of the package here that helps potential users
 know whether this package might be useful for them.
@@ -37,4 +38,3 @@ const like = 'sample';
 TODO: Tell users more about the package: where to find more information, how to
 contribute to the package, how to file issues, what response they can expect
 from the package authors, and more.
-# resizable_text
---
+++ lib/resizable_text.dart
@@ -0,0 +1,27 @@
+library resizable_text;
+
+// Body small - large, lead paragraph
+export 'src/body/body_large.dart';
+export 'src/body/body_medium.dart';
+export 'src/body/body_normal.dart';
+export 'src/body/body_small.dart';
+export 'src/body/lead_paragraph.dart';
+// Display1, Display2
+export 'src/display/display1.dart';
+export 'src/display/display2.dart';
+// H1 - H6
+export 'src/header/h1.dart';
+export 'src/header/h2.dart';
+export 'src/header/h3.dart';
+export 'src/header/h4.dart';
+export 'src/header/h5.dart';
+export 'src/header/h6.dart';
+// Label xs - xl
+export 'src/label/label_extra_large.dart';
+export 'src/label/label_extra_small.dart';
+export 'src/label/label_large.dart';
+export 'src/label/label_normal.dart';
+export 'src/label/label_small.dart';
+
+export 'src/resizable/resizable_text.dart' show ResizableText;
+export 'src/resizable/resizable_text_style.dart' show ResizableTextStyle;
